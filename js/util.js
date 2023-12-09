2e87b31849259547ea22b5ba96017cb2cb7bee4c
Kate <eaundesova@gmail.com>
2023-12-09 12:10:21 +0300

    Вносит корректировки согласно замечаниям

--- js/util.js
+++ js/util.js
@@ -1,3 +1,5 @@
+import { QUANTITY_NUMBERS } from './data.js';
+
 const getRandomInteger = (a = 0, b = 100) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
@@ -8,11 +10,11 @@ const getRandomInteger = (a = 0, b = 100) => {
 
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
-const getRandomLocation = (elements) => (Math.random() * (elements.max - elements.min) + elements.min).toFixed(5);
+const getRandomLocation = (elements) => (Math.random() * (elements.max - elements.min) + elements.min).toFixed(QUANTITY_NUMBERS);
 
-const createUniqArray = (elements) => {
+const createArrayWithRandomUniqElements = (elements) => {
   const objects = new Set(Array.from({ length: getRandomInteger(0, elements.length) }, () => getRandomArrayElement(elements)));
   return Array.from(objects);
 };
 
-export { getRandomInteger, getRandomArrayElement, getRandomLocation, createUniqArray };
+export { getRandomInteger, getRandomArrayElement, getRandomLocation, createArrayWithRandomUniqElements };
