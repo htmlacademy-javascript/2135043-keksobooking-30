9d39391802831989ee9d85434cb216de6f89c8d5
Kate <eaundesova@gmail.com>
2024-01-20 10:29:00 +0300

    Корректирует разметку согласно замечаниям

--- js/filter-sort.js
+++ js/filter-sort.js
@@ -6,14 +6,14 @@ const priceSelect = filtersForm.querySelector('#housing-price');
 const roomsSelect = filtersForm.querySelector('#housing-rooms');
 const guestsSelect = filtersForm.querySelector('#housing-guests');
 
-const filterSimilarMarkers = (points, markersList) => points
+const filterSimilarMarkers = (points, featuresList) => points
   .filter(({ offer }) => (typeSelect.value === DEFAULT_VALUE || offer.type === typeSelect.value))
   .filter(({ offer }) => (offer.price >= PRICE_TYPES[priceSelect.value].min && offer.price <= PRICE_TYPES[priceSelect.value].max))
   .filter(({ offer }) => (roomsSelect.value === DEFAULT_VALUE || offer.rooms === Number(roomsSelect.value)))
   .filter(({ offer }) => (guestsSelect.value === DEFAULT_VALUE || offer.guests === Number(guestsSelect.value)))
   .filter(({ offer }) => (
-    markersList.length === 0 || (offer.markers && markersList.every(
-      (marker) => offer.markers.includes(marker))
+    featuresList.length === 0 || (offer.features && featuresList.every(
+      (feature) => offer.features.includes(feature))
     ))
   )
   .slice(MIN_ADS_COUNT, SIMILAR_ADS_COUNT);
